dce19ac96713901958c52256ea73e06c832635b2
Jose Manuel <jmpmscorp@hotmail.com>
2022-10-26 01:23:00 +0200

    Update files doc

--- include/sdi12_bus.h
+++ include/sdi12_bus.h
@@ -9,7 +9,7 @@
 extern "C"
 {
 #endif
-    #define SDI12_DEFAULT_RESPONSE_TIMEOUT (1000) // Milliseconds
+#define SDI12_DEFAULT_RESPONSE_TIMEOUT (1000) // Milliseconds
 
     typedef struct sdi12_bus sdi12_bus_t;
 
@@ -29,35 +29,34 @@ extern "C"
 
     /**
      * @brief Send command over the bus and waits ONLY for first response line (first <LF><CR> found).
-     * 
+     *
      * @details AM!, AMx!, aMC!, aMCx!, aV! commands require a service request.
-     *          When service request command is issued, timeout is used for wait 'atttn', 'atttnn' or 'atttnnn' response line. However, send cmd proccess can take more time due to
-     *          'ttt' seconds. Function automatically calculates elapse time and waits for it. If there is no response, ESP_TIMEOUT is returned. On the other side,
-     *          response (device address) is checked with cmd address. If comparison fails, ESP_FAIL is returned. 
-     * 
-     * 
-     * 
-     * @param bus[in]                   bus object
-     * @param cmd[in]                   cmd to send
-     * @param out_buffer[out]           buffer to save response
-     * @param out_buffer_legnth[out]    
-     * @param check_crc[in]             
-     * @param timeout[in]               time to wait for response
-     * 
+     * When service request command is issued, timeout is used for wait 'atttn', 'atttnn' or 'atttnnn' response line. However, send cmd proccess can
+     * take more time due to 'ttt' seconds. Function automatically calculates elapse time and waits for it. If there is no response, ESP_TIMEOUT is returned. On
+     * the other side, response (device address) is checked with cmd address. If comparison fails, ESP_FAIL is returned.
+     *
+     *
+     *
+     * @param[in] bus                   bus object
+     * @param[in] cmd                   cmd to send
+     * @param[in] check_crc             true if crc check is needed. false otherwise
+     * @param[out] out_buffer           buffer to save response
+     * @param[out] out_buffer_length    response buffer length
+     * @param[in] timeout               time to wait for response
+     *
      * @return esp_err_t
      *      ESP_OK
      *      ESP_ERR_TIMEOUT
-     *      ESP_ERR_FAIL  
+     *      ESP_ERR_FAIL
      *      ESP_ERR_INVALID_ARG
      *      ESP_ERR_INVALID_SIZE
      */
     esp_err_t sdi12_bus_send_cmd(sdi12_bus_t *bus, const char *cmd, bool crc, char *out_buffer, size_t out_buffer_length, uint32_t timeout);
 
-
     /**
      * @brief Deallocate and free bus resources
      *
-     * @param bus[in]       bus object to deallocate
+     * @param[in] bus       bus object to deallocate
      * @return esp_err_t
      */
 
@@ -66,8 +65,8 @@ extern "C"
     /**
      * @brief Initialize SDI12 bus object
      *
-     * @param config            See sdi12_bus_config_t
-     * @return sdi12_bus_t*     NULL if error. SDI12 object if success.
+     * @param[in] config            See sdi12_bus_config_t
+     * @return sdi12_bus_t*         NULL if error. SDI12 object pointer if success.
      */
     sdi12_bus_t *sdi12_bus_init(sdi12_bus_config_t *config);
 
--- include/sdi12_dev.h
+++ include/sdi12_dev.h
@@ -16,32 +16,273 @@ extern "C"
 
     typedef struct sdi12_dev sdi12_dev_t;
 
+    /**
+     * @brief Get address stored in object
+     *
+     * @note No bus interaction
+     *
+     * @param[in] dev   Device object
+     * @return char
+     */
     char sdi12_dev_get_address(sdi12_dev_t *dev);
+
+    /**
+     * @brief Get SDI12 version stored in object
+     *
+     * @note No bus interaction
+     *
+     * @param[in] dev   Device object
+     * @return sdi12_version_t
+     */
     sdi12_version_t sdi12_dev_get_sdi_version(sdi12_dev_t *dev);
+
+    /**
+     * @brief Get address stored in object
+     *
+     * @note No bus interaction
+     *
+     * @param[in] dev   Device object
+     * @return char
+     */
     char *sdi12_dev_get_vendor_id(sdi12_dev_t *dev);
+
+    /**
+     * @brief Get model stored in object
+     *
+     * @note No bus interaction
+     *
+     * @param[in] dev   Device object
+     * @return char *
+     */
     char *sdi12_dev_get_model(sdi12_dev_t *dev);
+
+    /**
+     * @brief Get model version stored in object
+     *
+     * @note No bus interaction
+     *
+     * @param[in] dev   Device object
+     * @return char *
+     */
     char *sdi12_dev_get_model_version(sdi12_dev_t *dev);
+
+    /**
+     * @brief Get optional field stored in object
+     *
+     * @note No bus interaction
+     *
+     * @param[in] dev   Device object
+     * @return char *
+     */
     char *sdi12_dev_get_optional_info(sdi12_dev_t *dev);
 
+    /**
+     * @brief Send a! command.
+     *
+     * @param[in] dev       Device object
+     * @param[in] timeout   Time to wait for response
+     * @return esp_err_t
+     *      ESP_OK if no error
+     *      ESP_ERR_TIMEOUT if timeout expires
+     *      ESP_ERR_INVALID_ARG if invalid dev
+     *      ESP_ERR_FAIL any other error
+     */
     esp_err_t sdi12_dev_acknowledge_active(sdi12_dev_t *dev, uint32_t timeout);
+
+    /**
+     * @brief Send aAb! command.
+     *
+     * @param[in] dev           Device object
+     * @param[in] new_address   New device address
+     * @param[in] timeout
+     * @return esp_err_t
+     *      ESP_OK if no error
+     *      ESP_ERR_TIMEOUT if timeout expires
+     *      ESP_ERR_INVALID_ARG if invalid dev or invalid new address
+     *      ESP_ERR_FAIL any other error
+     */
     esp_err_t sdi12_dev_change_address(sdi12_dev_t *dev, char new_address, uint32_t timeout);
+
+    /**
+     * @brief Send aI! command.
+     *
+     * @details this command reads and store on memory information of sensor. If no out_buffer is passed, command only stores information. You can read stored
+     * information with provided getters.
+     *
+     * @param[in] dev                   Device object
+     * @param[out] out_buffer           Buffer for response
+     * @param[out] out_buffer_length    Response buffer length
+     * @param[in] timeout               Time to wait for response
+     * @return esp_err_t
+     *      ESP_OK if no error
+     *      ESP_ERR_TIMEOUT if timeout expires
+     *      ESP_ERR_INVALID_ARG if invalid dev
+     *      ESP_ERR_FAIL any other error
+     */
     esp_err_t sdi12_dev_read_identification(sdi12_dev_t *dev, char *out_buffer, size_t out_buffer_length, uint32_t timeout);
+
+    /**
+     * @brief Send ?! command.
+     *
+     * @warning This command could be used if ONLY ONE device is connected on bus.
+     *
+     * @param[in] dev       Device object
+     * @param[out] address  Returned sensor address
+     * @param[in] timeout   Time to wait for response
+     * @return esp_err_t
+     *      ESP_OK if no error
+     *      ESP_ERR_TIMEOUT if timeout expires
+     *      ESP_ERR_INVALID_ARG if invalid dev or invalid new address
+     *      ESP_ERR_FAIL any other error
+     */
     esp_err_t sdi12_dev_address_query(sdi12_dev_t *dev, char *address, uint32_t timeout);
+
+    /**
+     * @brief Send aMx! or aMCx! command.
+     *
+     * @details To send aM! or aMC! command, set m_index 0.
+     *
+     * @param[in] dev           Device object
+     * @param[in] m_index       x on aMx!
+     * @param[in] crc           True to send CRC version 'aMCx!'. False to send 'aMx!'
+     * @param[out] n_params     Number of values returned on measurements. n on response 'atttn'
+     * @param[in] timeout       Time to wait for response
+     * @return esp_err_t
+     *      ESP_OK if no error
+     *      ESP_ERR_TIMEOUT if timeout expires
+     *      ESP_ERR_INVALID_ARG if invalid dev or m_index > 9
+     *      ESP_ERR_FAIL any other error
+     */
     esp_err_t sdi12_dev_start_measurement(sdi12_dev_t *dev, uint8_t m_index, bool crc, uint8_t *n_params, uint32_t timeout);
+
+    /**
+     * @brief Send aDx! command.
+     *
+     * @details To send aD!, set d_index 0.
+     *
+     * @param[in] dev                   Device object
+     * @param[in] d_index               x on aDx!
+     * @param[in] crc                   True to check CRC. False otherwise.
+     * @param[out] out_buffer           Buffer to store response
+     * @param[out] out_buffer_length    Response buffer length
+     * @param[in] timeout               Time to wait for response
+     * @return esp_err_t
+     *      ESP_OK if no error
+     *      ESP_ERR_TIMEOUT if timeout expires
+     *      ESP_ERR_INVALID_ARG if invalid dev or d_index > 9
+     *      ESP_ERR_FAIL any other error
+     */
     esp_err_t sdi12_dev_read_data(sdi12_dev_t *dev, uint8_t d_index, bool crc, char *out_buffer, size_t out_buffer_length, uint32_t timeout);
+
+    /**
+     * @brief Send aV! command.
+     *
+     * @param[in] dev           Device object
+     * @param[out] n_params     Number of values returned on measurements. n on response 'atttn'
+     * @param[in] timeout       Time to wait for response
+     * @return esp_err_t
+     *      ESP_OK if no error
+     *      ESP_ERR_TIMEOUT if timeout expires
+     *      ESP_ERR_INVALID_ARG if invalid dev
+     *      ESP_ERR_FAIL any other error
+     */
     esp_err_t sdi12_dev_start_verification(sdi12_dev_t *dev, uint8_t *n_params, uint32_t timeout);
+
+    /**
+     * @brief Send aCx! or aCCx! command.
+     * 
+     * @details To send aC! or aCC! command, set c_index 0.
+     * 
+     * @param[in] dev       Device object
+     * @param[in] c_index   x on aCx! or aCCx!
+     * @param[in] crc       True to send CRC version 'aCCx!'. False to send 'aCx!'
+     * @param[out] n_params Number of values returned on measurements. n on response 'atttn'
+     * @param[in] timeout   Time to wait for response
+     * @return esp_err_t
+     *      ESP_OK if no error
+     *      ESP_ERR_TIMEOUT if timeout expires
+     *      ESP_ERR_INVALID_ARG if invalid dev
+     *      ESP_ERR_FAIL any other error
+     */
     esp_err_t sdi12_dev_start_concurrent_measurement(sdi12_dev_t *dev, uint8_t c_index, bool crc, uint8_t *n_params, uint32_t timeout);
+
+    /**
+     * @brief Send aRx! command.
+     * 
+     * @details To send aR!, set d_index 0.
+     * 
+     * @param[in] dev                   Device object
+     * @param[in] r_index               x on aRx!
+     * @param[in] crc                   True to check CRC. False otherwise
+     * @param[out] out_buffer           Buffer to store response
+     * @param[out] out_buffer_length    Response buffer length
+     * @param[in] timeout               Time to wait for response
+     * @return esp_err_t
+     *      ESP_OK if no error
+     *      ESP_ERR_TIMEOUT if timeout expires
+     *      ESP_ERR_INVALID_ARG if invalid dev
+     *      ESP_ERR_FAIL any other error
+     */
     esp_err_t sdi12_dev_read_continuos_measurement(sdi12_dev_t *dev, uint8_t r_index, bool crc, char *out_buffer, size_t out_buffer_length, uint32_t timeout);
 
     // TODO implement
     // esp_err_t sdi12_dev_start_high_volume_ascii_measurement(sdi12_dev_t *dev, uint8_t measures_index, uint8_t *n_params);
     // esp_err_t sdi12_dev_start_high_volume_bin_measurement(sdi12_dev_t *dev, uint8_t measures_index, uint8_t *n_params);
 
+    /**
+     * @brief Send any identity command, aIX!
+     * 
+     * @details Function add I automatically. You only need to provide which command you want to send. i.e. To send aIMC!, you need to pass MC through cmd.
+     * 
+     * @param[in] dev           Device object
+     * @param[in] cmd           CMD to send 
+     * @param[out] n_params     Number of values returned on measurements. n on response 'atttn', nn on 'atttnn' or nnn on 'atttnnn'
+     * @param[in] timeout       Time to wait for response
+     * @return esp_err_t 
+     * @return esp_err_t
+     *      ESP_OK if no error
+     *      ESP_ERR_TIMEOUT if timeout expires
+     *      ESP_ERR_INVALID_ARG if invalid dev or cmd length > 3
+     *      ESP_ERR_FAIL any other error
+     */
     esp_err_t sdi12_dev_read_identify_cmd(sdi12_dev_t *dev, const char *cmd, uint8_t *n_params, uint32_t timeout);
 
+    /**
+     * @brief Send any command not provided in this api.
+     * 
+     * @details Take in mind that device address and '!' is append automatically to cmd. i.e. To send aHB!, set cmd as HB  
+     * 
+     * @param[in] dev                   Device object
+     * @param[in] cmd                   Cmd to send 
+     * @param[in] crc                   Set to true if response needs to check CRC. False otherwise
+     * @param[out] out_buffer           Buffer to store response
+     * @param[out] out_buffer_length    Response buffer length 
+     * @param[in] timeout               Time to wait for response
+     * @return esp_err_t
+     *      ESP_OK if no error
+     *      ESP_ERR_TIMEOUT if timeout expires
+     *      ESP_ERR_INVALID_ARG if invalid dev
+     *      ESP_ERR_FAIL any other error
+     */
     esp_err_t sdi12_dev_extended_cmd(sdi12_dev_t *dev, const char *cmd, bool crc, char *out_buffer, size_t out_buffer_length, uint32_t timeout);
 
+    /**
+     * @brief Free device memory resources
+     * 
+     * @param[in] dev   Device object
+     */
     void sdi12_dev_deinit(sdi12_dev_t *dev);
+
+    /**
+     * @brief Allocate resources for device.
+     * 
+     * @details Before allocate device resources and if address is different from '?', acknowledge active command is sent. If response is OK, device object is returned and NULL if any error on response.
+     * On other hand, if address is '?', instead of acknowledge active, address query command is sent.  
+     * 
+     * @param[in] bus           SDI12 bus object 
+     * @param[in] address       Device address address or '?' to query it 
+     * @return sdi12_dev_t*     Device object or NULL
+     */
     sdi12_dev_t *sdi12_dev_init(sdi12_bus_t *bus, char address);
 
 #ifdef __cplusplus
